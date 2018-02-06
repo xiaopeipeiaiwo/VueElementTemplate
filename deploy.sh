c4500330fafdc43662ce3851272b3d4ade6a1914
胡小根 <hxg@haomo-studio.com>
2018-02-06 17:45:32 +0100

    实现了三张表的demo

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,3 @@
 
 npm run build:prod
 rsync -avz dist/* member@haomo-studio.com:/var/www/html/software_factory/vue-element/
-
-cmd="git clone xxx" && ret=`ps -ef|grep -v grep|grep ${cmd}` && if [ -z $ret ]; then echo '空字符串'; else echo '非空字符串'; fi
