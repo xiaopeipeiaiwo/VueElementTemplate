731c6b4c27347f2c842a33a680f738ab18e94c87
赵帅 <gedesiwen@haomo-studio.com>
2018-02-22 13:33:51 +0800

    codeing

--- src/views/haomo/components/details/HmComplexDetail.vue
+++ src/views/haomo/components/details/HmComplexDetail.vue
@@ -1,7 +1,7 @@
 <template>
   <el-row type="flex">
     <el-col :span="24" class="detail-content">
-      <h2 class="title">详情页面</h2>
+      <!--<h2 class="title">详情页面</h2>-->
       <el-form :data="detail" label-width="110px" status-icon style="width:80%;margin:0 auto">
         <el-form-item v-for="(column,index) in showColumns" :key="index" :label="column.name">
           <div>{{detail[column.codeCamel]}}</div>
