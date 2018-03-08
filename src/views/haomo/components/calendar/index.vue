baa0d8f6b12d6b4360b91017b0f14b7cd1a54b77
gaochao <54423923@qq.com>
2018-03-08 17:22:06 +0800

    a

---
+++ src/views/haomo/components/calendar/index.vue
@@ -0,0 +1,40 @@
+<template>
+	<div class="calender">
+	  <full-calendar :events="fcEvents" locale="en"></full-calendar>
+	</div>
+</template>
+
+<script>
+  import fullCalendar from 'vue-fullcalendar'
+  export default {
+    name: 'calender',
+    // 继承其他组件
+    extends: {},
+    // 使用其它组件
+    components: {
+      'full-calendar': fullCalendar
+    },
+    data() {
+      return {
+        fcEvents: [
+          {
+            title: 'Sunny Out of Office',
+            start: '2018-03-8',
+            end: '2018-03-8',
+            cssClass: 'aaa'
+          }
+        ]
+      }
+    },
+    computed: {
+    },
+    filters: {
+    },
+    created() {
+    },
+    methods: {}
+  }
+</script>
+<style scoped>
+</style>
+
